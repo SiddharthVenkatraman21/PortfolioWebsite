98eeb48d8ee0a871a0f674806a232e7f52f4b524
Siddharth Venkatraman <sid.venkatraman17@gmail.com>
2023-12-15 22:32:57 -0600

    Added typewriter effect to introductions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Array of translations for "Hello" in different languages
+    const helloSlide = document.getElementById('helloSlide');
     const helloTranslations = [
       'Hello!', // Default
       'Bonjour!', // French
@@ -10,16 +10,40 @@ document.addEventListener('DOMContentLoaded', function () {
       'こんにちは!', // Japanese
       '안녕하세요!', // Korean
     ];
-  
-    // Function to update the hello slide text
-    function updateHelloSlide() {
-        const helloSlide = document.getElementById('helloSlide');
-        const currentHelloIndex = helloTranslations.indexOf(helloSlide.innerText);
-        const nextHelloIndex = (currentHelloIndex + 1) % helloTranslations.length;
-        helloSlide.innerText = helloTranslations[nextHelloIndex];
+
+    let translationIndex = 0;
+
+    function typeWriter(text, index, callback) {
+      if (index < text.length) {
+        helloSlide.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(function () {
+          typeWriter(text, index, callback);
+        }, 75); // Typing speed (milliseconds per character)
+      } else {
+        // After typing, execute the callback (usually the erasing function)
+        setTimeout(callback, 1000);
       }
-    
-      // Update the hello slide every second
-      setInterval(updateHelloSlide, 1000);
-    });
-  
+    }
+
+    function eraseText() {
+        const currentText = helloSlide.textContent;
+        let index = currentText.length - 1;
+
+        const eraseInterval = setInterval(function () {
+          if (index >= 0) {
+            helloSlide.textContent = currentText.slice(0, index);
+            index--;
+          } else {
+            clearInterval(eraseInterval);
+            // Move to the next translation
+            translationIndex = (translationIndex + 1) % helloTranslations.length;
+            // Type the next translation
+            typeWriter(helloTranslations[translationIndex], 0, eraseText);
+          }
+        }, 75); // Erasing speed (milliseconds per character)
+      }
+
+    // Start the typewriter effect with the default translation
+    typeWriter(helloTranslations[translationIndex], 0, eraseText);
+  });
